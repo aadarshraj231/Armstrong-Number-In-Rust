06580be010838d92e39b066bff2026ff0dab9906
Aadarsh Raj <58441280+aadarshraj231@users.noreply.github.com>
2019-12-04 15:08:16 +0530

    Update armstrong_strong.rs

--- armstrong_number/tests/armstrong_strong.rs
+++ armstrong_number/tests/armstrong_strong.rs
@@ -3,10 +3,10 @@ use armstrong_numbers::*;
 
 #[test]
 fn test_single_digit_numbers_are_armstrong_numbers() {
-    assert!(is_armstrong_number(5))
+    assert!(main(5))
 }
 
 #[test]
 fn test_there_are_no_2_digit_armstring_numbers() {
-    assert!(!is_armstrong_number(10))
+    assert!(!main(10))
 }
